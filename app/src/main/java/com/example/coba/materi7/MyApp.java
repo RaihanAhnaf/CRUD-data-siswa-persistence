8efa614a449557e4d246b14d6d0ac5aaa2221fe8
RaihanAhnaf <mz.raihan11@gmail.com>
2019-04-15 07:04:39 +0700

    first commit

---
+++ app/src/main/java/com/example/coba/materi7/MyApp.java
@@ -0,0 +1,14 @@
+package com.example.coba.materi7;
+
+import android.app.Application;
+import androidx.room.Room;
+
+public class MyApp extends Application {
+    public static AppDatabase db;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        db = Room.databaseBuilder (getApplicationContext(),
+                AppDatabase.class, "siswa.db").allowMainThreadQueries().build();
+    }
+}
